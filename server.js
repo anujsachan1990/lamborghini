8b41476db6cbbf4ab9435302d72810ef0c78bf47
Anuj Sachan <asachan@sapient.com>
2017-03-28 14:07:47 +0530

    read content from Remote Website using Https

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log(333333333);
+import https from 'https';
+
+https.get("https://www.sunsuper.com.au",res=>{
+	console.log("response code",res.statusCode);
+	res.on('data',chuck=>{
+		console.log(chuck.toString());
+	})
+});
+
