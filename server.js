1c84416bf5364a54628e843bcb6125313fe324aa
Anuj Sachan <asachan@sapient.com>
2017-03-28 20:18:25 +0530

    added server side templates Ejs

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 import config from './config';
 import express from 'express';
 import apiRouter from './api';
+import axios from "axios";
+
 const server = express();
 
 server.use(express.static('public'));
 server.use("/api",apiRouter);
+server.set("view engine",'ejs');
+server.get("/",(req,res) => {
 
+	res.render('index',{
+
+		name:"Anuj Sachan"
+
+	});
+});	
 
 server.listen(config.port,()=>{
 	console.log("express server is listing on configured port "+config.port);
